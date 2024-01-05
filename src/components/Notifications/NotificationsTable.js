25563a68082d251a2a6efcbe182413b9f56b7223
Muhammad Bilal <bilal.xbt@gmail.com>
2024-01-05 16:39:56 +0500

    design set

--- src/components/Notifications/NotificationsTable.js
+++ src/components/Notifications/NotificationsTable.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-//import Table from 'react-bootstrap/Table';
 import { Image, Col, Dropdown } from 'react-bootstrap';
 import Profile from '../../assets/images/icons/table.svg';
 import threeDots from '../../assets/images/icons/CardIcons/threeDots.svg';
@@ -53,45 +52,6 @@ const NotificationsTable = () => {
   const [selectedRow, setSelectedRow] = useState(null);
 
   return (
-    // <div>
-    //   {data.map((group, groupIndex) => (
-    //     <div key={groupIndex} className="notificatons row">
-    //       <div className="separator">
-    //         <div className="line"></div>
-    //         <p className="ms-3 me-3 mt-3">{group.date}</p>
-    //         <div className="line"></div>
-    //       </div>
-    //       {group.notifications.map((notification, index) => (
-    //         <Col
-    //           md={6}
-    //           key={index}
-    //           onClick={() => {
-    //             if (selectedRow === index) {
-    //               setSelectedRow(null);
-    //             } else {
-    //               setSelectedRow(index);
-    //             }
-    //           }}
-    //           className="my-2 py-1">
-    //           <div
-    //             className={`notification-bar p-3 ${selectedRow === index ? 'selected-row' : ''} ${
-    //               notification.status.toLowerCase() === 'paid' ? 'read' : 'unread'
-    //             }`}>
-    //             <Image
-    //               src={notification.picture}
-    //               className="rounded-2"
-    //               style={{ width: '56px', height: '56px' }}
-    //             />
-    //             <div className="d-flex flex-column align-items-start justify-content-center ms-3 p-0 w-100">
-    //               {/* Continue rendering the notification details */}
-    //               {/* ... */}
-    //             </div>
-    //           </div>
-    //         </Col>
-    //       ))}
-    //     </div>
-    //   ))}
-    // </div>
     <div>
       {data.map((group, groupIndex) => (
         <div key={groupIndex} className="notificatons row">
@@ -116,50 +76,52 @@ const NotificationsTable = () => {
                 className={`notification-bar p-3 ${selectedRow === index ? 'selected-row' : ''} ${
                   notification.status.toLowerCase() === 'paid' ? 'read' : 'unread'
                 }`}>
-                <Image
-                  src={notification.picture}
-                  className="rounded-2"
-                  style={{ width: '56px', height: '56px' }}
-                />
-                <div className="d-flex flex-column align-items-start justify-content-center ms-3 p-0 w-100">
-                  <div className="d-flex align-items-start justify-content-between w-100">
-                    <div>
-                      <p className="m-0 custom-heading-font fw-bold text-capitalize">
-                        {notification.name}
-                      </p>
-                      <p className="user-name text-14 grey m-0">{notification.Bookingtime}</p>
-                    </div>
-                    <div className="threeDots-dropdown">
-                      <Dropdown>
-                        <Dropdown.Toggle id="dropdown-basic" className="border-0">
-                          <Image alt="gallery" src={threeDots} className=" p-2" />
-                        </Dropdown.Toggle>
+                <div className="d-flex align-items-start">
+                  <Image
+                    src={notification.picture}
+                    className="rounded-2"
+                    style={{ width: '56px', height: '56px' }}
+                  />
+                  <div className="d-flex flex-column align-items-start justify-content-center ms-3 p-0 w-100">
+                    <div className="d-flex align-items-start justify-content-between w-100">
+                      <div>
+                        <p className="m-0 custom-heading-font fw-bold text-capitalize">
+                          {notification.name}
+                        </p>
+                        <p className="user-name text-14 grey m-0">{notification.Bookingtime}</p>
+                      </div>
+                      <div className="threeDots-dropdown">
+                        <Dropdown>
+                          <Dropdown.Toggle id="dropdown-basic" className="border-0">
+                            <Image alt="gallery" src={threeDots} className=" p-2" />
+                          </Dropdown.Toggle>
 
-                        <Dropdown.Menu></Dropdown.Menu>
-                      </Dropdown>
+                          <Dropdown.Menu></Dropdown.Menu>
+                        </Dropdown>
+                      </div>
                     </div>
+                    {notification.type === 'chat' ? (
+                      <div className="d-flex flex-column align-items-start justify-content-center pt-3 gap-1">
+                        <p className="m-0 user-name text-14 font-700 grey">Message</p>
+                        <span className="custom-lineheight"></span>
+                      </div>
+                    ) : (
+                      <div className="d-flex align-items-start justify-content-between gap-3 w-100 pt-3 flex-wrap">
+                        <div className="d-flex flex-column align-items-start justify-content-center gap-1">
+                          <p className="m-0 user-name text-14 font-700 grey">Status</p>
+                          <p className="m-0 font-14-20 text-capitalize">{notification.status}</p>
+                        </div>
+                        <div className="d-flex flex-column align-items-start justify-content-center gap-1">
+                          <p className="m-0 user-name text-14 font-700 grey">Category</p>
+                          <p className="m-0 font-14-20">{notification.spacetype}</p>
+                        </div>
+                        <div className="d-flex flex-column align-items-start justify-content-center gap-1">
+                          <p className="m-0 user-name text-14 font-700 grey">Total Amount</p>
+                          <p className="m-0 font-14-20">${notification.price}</p>
+                        </div>
+                      </div>
+                    )}
                   </div>
-                  {notification.type === 'chat' ? (
-                    <div className="d-flex flex-column align-items-start justify-content-center pt-3 gap-1">
-                      <p className="m-0 user-name text-14 font-700 grey">Message</p>
-                      <span className="custom-lineheight"></span>
-                    </div>
-                  ) : (
-                    <div className="d-flex align-items-start justify-content-between gap-3 w-100 pt-3 flex-wrap">
-                      <div className="d-flex flex-column align-items-start justify-content-center gap-1">
-                        <p className="m-0 user-name text-14 font-700 grey">Status</p>
-                        <p className="m-0 font-14-20 text-capitalize">{notification.status}</p>
-                      </div>
-                      <div className="d-flex flex-column align-items-start justify-content-center gap-1">
-                        <p className="m-0 user-name text-14 font-700 grey">Category</p>
-                        <p className="m-0 font-14-20">{notification.spacetype}</p>
-                      </div>
-                      <div className="d-flex flex-column align-items-start justify-content-center gap-1">
-                        <p className="m-0 user-name text-14 font-700 grey">Total Amount</p>
-                        <p className="m-0 font-14-20">${notification.price}</p>
-                      </div>
-                    </div>
-                  )}
                 </div>
               </div>
             </Col>
